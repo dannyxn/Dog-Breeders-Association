171175af6b7781b3976c5571d97156a8e5d8bba5
DESKTOP-SOT8G9N\dawid <dawid.chara.9@gmail.com>
2020-12-06 22:03:42 +0100

    Some moving changes in index view + templates & static content

--- main/main.py
+++ main/main.py
@@ -1,9 +1,10 @@
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
+
 bp = Blueprint('main', __name__, url_prefix='/')
 
 
 @bp.route('/')
 def index():
-    return render_template('main/index.html')
+    return render_template('index.html')
